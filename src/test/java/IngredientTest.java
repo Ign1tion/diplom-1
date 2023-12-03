fb9fd524df93b6513f1848cf868bb273bcef70cc
Ign1tion <ivegotahangover@mail.ru>
2023-12-04 02:50:18 +0300

    fxd

--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -1,49 +1,31 @@
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
 import static praktikum.IngredientType.FILLING;
 import static praktikum.IngredientType.SAUCE;
 
-@RunWith(Parameterized.class)
 public class IngredientTest {
-    private final IngredientType type;
-    private final String name;
-    private final float price;
-    Ingredient ingredient;
-    public IngredientTest(IngredientType type, String name, float price){
-        this.type = type;
-        this.name = name;
-        this.price = price;
+    @Test
+    public void getPriceTest() {
+        Ingredient ingredient = new Ingredient(FILLING, "cutlet", 100f);
+        float expected = 100f;
+        Assert.assertEquals(expected, ingredient.getPrice(), 0);
     }
-    @Parameterized.Parameters
-    public static Object[][] makeIngredients(){
-        return new Object[][]{
-                {FILLING, "cutlet", 100f},
-                {SAUCE, "sour cream", 200000000000000000000f },
-                {FILLING, "!@#$%^&*(", -1f },
-                {FILLING, "", 0f},
-                {FILLING, "     ", Float.MAX_VALUE},
-               {FILLING, "soooooooooooooooooooooooManySymbols", 1f}
-    };
-}
-@Test
-    public void getPriceTest(){
-        ingredient = new Ingredient(type, name, price);
-    Assert.assertEquals(price, ingredient.getPrice(), 0);
-}
-@Test
-    public void getNameTest(){
-        ingredient = new Ingredient(type, name, price);
-        Assert.assertEquals(name, ingredient.getName());
-}
-@Test
-    public void getTypeTest(){
-        ingredient = new Ingredient(type, name, price);
-        Assert.assertEquals(type, ingredient.getType());
-}
+
+    @Test
+    public void getNameTest() {
+        Ingredient ingredient = new Ingredient(SAUCE, "sausage", 1000000f);
+        String expected = "sausage";
+        Assert.assertEquals(expected, ingredient.getName());
+    }
+
+    @Test
+    public void getTypeTest() {
+        Ingredient ingredient = new Ingredient(FILLING, "bacon", 500f);
+        IngredientType expected = FILLING;
+        Assert.assertEquals(expected, ingredient.getType());
+    }
 }
 
